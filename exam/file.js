a164778da78b65bd2064c2ab27fd309189ebdf12
JavierIglesias27 <jiglesro7@gmail.com>
2022-05-09 17:18:30 +0200

    save6

---
+++ exam/file.js
@@ -0,0 +1,34 @@
+//body
+document.getElementsByTagName("body")[0].style.backgroundColor="#ttt";
+
+//fielsed
+document.getElementsByTagName("fieldset")[0].style.backgroundColor="grey";
+document.getElementsByTagName("fieldset")[0].style.border ="4px solid black";
+//legend
+document.getElementsByTagName("legend")[0].style.backgroundColor="red";
+document.getElementsByTagName("legend")[0].style.textAlign="center";
+document.getElementsByTagName("legend")[0].style.border ="2px solid grey";
+document.getElementsByTagName("legend")[0].innerHTML += "asd";
+
+//label 1
+document.getElementById("tag_pos1").style.backgroundColor="blue";
+document.getElementById("tag_pos1").style.color = "white";
+document.getElementById("tag_pos1").style.fontFamily = "arial";
+//input1
+document.getElementsByTagName("input")[0].style.color="red";
+//document.getElementsByTagName("input")[0].style.backgroundColor="violet";
+
+//label2
+document.getElementsByTagName("label")[1].style.backgroundColor="red";
+document.getElementsByTagName("label")[1].style.color="white";
+document.getElementsByTagName("label")[1].style.fontSize="20px";
+document.getElementsByTagName("label")[1].style.border="2px solid black";
+document.getElementsByTagName("label")[1].style.textTransform="lowercase";
+//label3 nombres y apellidos
+
+document.getElementsByTagName("label")[2].classList.add("orange");
+//label45-codigo
+document.getElementsByTagName("label")[3].classList.add("green");
+document.getElementsByTagName("label")[3].style.textTransform="uppercase";
+document.getElementsByTagName("label")[4].classList.add("blue");
+ 
